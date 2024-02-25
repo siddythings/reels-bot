85a6774c898355e31dd9e9749320d97f193d3c18
siddhart <s.vikram@enact-systems.com>
2024-02-25 08:49:51 +0530

    setting up the inc24 class

---
+++ services/inc24/inc24.py
@@ -0,0 +1,15 @@
+from services.provider import ServiceProvider
+
+
+class INC24(ServiceProvider):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_posts(self):
+        pass
+
+    def get_images(self):
+        pass
+
+    def create_video(self):
+        pass
