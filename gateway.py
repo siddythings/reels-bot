6d3d8bda8c48d8dbca2c7fdcae441b77bba5c39c
siddhart <s.vikram@enact-systems.com>
2024-02-25 21:25:42 +0530

    update gateway methods

--- gateway.py
+++ gateway.py
@@ -10,8 +10,11 @@ class Gateway:
     def get_posts(self, number_of_page):
         return self._provider.get_posts(number_of_page)
 
-    def get_images(self):
-        return self._provider.get_images()
+    def get_images(self, screen_objects):
+        return self._provider.get_images(screen_objects)
+
+    def create_audio(self, screen_objects):
+        return self._provider.create_audio(screen_objects)
 
     def create_video(self):
         return self._provider.create_video()
